63f7bb16d216bd571a1fd749c9c8397da27d3660
Jagmeet Kaur <105890187+jagmeetkaurtiet@users.noreply.github.com>
2024-06-29 20:18:43 +0000

    initial comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,55 @@
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
 function App() {
+  const [select, isSelected]=useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+   <>
+   <ul className="nav nav-tabs">
+  <li class="nav-item">
+    <button className={select===0?"active":""} onClick={()=>isSelected(0)} aria-current="page">Home</button>
+  </li>
+  <li class="nav-item">
+    <button className={select===1?"active":""}  onClick={()=>isSelected(1)} >Career</button>
+  </li>
+  <li class="nav-item">
+    <button class={select===2?"active":""}  onClick={()=>isSelected(2)}>About</button>
+  </li>
+  <li class="nav-item">
+    <button class={select===3?"active":""}  onClick={()=>isSelected(3)}>Help</button>
+  </li>
+</ul>
+<div>
+<ul>{content[select].map((item)=>(<li key={item}>{item}</li>))}</ul>
+</div>
+</>
   );
 }
 
